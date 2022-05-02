53269474a8e395740acf72ee675da8d20d3ab704
Leonardo Oliveira e Silva <leonardo.s@compasso.com.br>
2022-05-02 17:31:18 -0300

    new-login-component

--- ui.frontend/src/components/LoginChallenge/LoginChallenge.vue
+++ ui.frontend/src/components/LoginChallenge/LoginChallenge.vue
@@ -1,19 +1,58 @@
 <template>
   <div id="divLogin">
-    <p> Olá, </p>
-    <p> Para continuar navegando de forma segura, efetue o login na rede. </p>
-    <p> Login </p>
 
-    <div id="divInputs">
+    <div id="leftDiv">
 
+      <div id="textSection" >
+        <p id="__p-1"> {{title}} </p>
+        <p id="__p-2"> {{text}} </p>
+        <p id="__p-3"> {{loginTitle}} </p>
+        <input type="text" class="inputCommon" placeholder="Usuário" id="inputLoginUser"/>
+        <input type="text" class="inputCommon" placeholder="Senha" id="inputLoginPassword"/>
+
+        <p id="errorMessage">{{errorMessage}}</p>
+
+        <button id="btnLogin">{{buttonName}}</button>
+      </div>
     </div>
+
+    <div id="rightDiv">
+      <img id="imageUrl" :src="imageUrl"/>
+    </div>
+
   </div>
+
 </template>
 
 <script>
 // Imports
 export default {
-  name: 'LoginChallenge'
+  name: 'LoginChallenge',
+  props: {
+    title: {
+      type: String
+    },
+    text: {
+      type: String
+    },
+    loginTitle: {
+      type: String
+    },
+    buttonName: {
+      type: String
+    },
+    // imageUrl: {
+    //   type: String
+    // },
+    errorMessage: {
+      type: String
+    }
+  },
+  data () {
+    return {
+      imageUrl: '/content/dam/vue/loginLogo.png'
+    }
+  }
 }
 </script>
 
