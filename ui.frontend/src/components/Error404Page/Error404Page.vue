d87b48c7072997bcf9acc5bd82b769aa046175e5
Leonardo Oliveira e Silva <leonardo.s@compasso.com.br>
2022-05-16 16:53:00 -0300

    refactoring-404-page

---
+++ ui.frontend/src/components/Error404Page/Error404Page.vue
@@ -0,0 +1,155 @@
+<template>
+<div id="container-main">
+  <header>{{ headerTitle }}</header>
+  <main>
+    <div id="container-left">
+      <img :src="imageUrl" alt="Scarecrow image" />
+    </div>
+    <div id="container-right">
+      <h1> {{ title }} </h1>
+      <p> {{ text }} </p>
+      <button> {{ buttonLabel }} </button>
+    </div>
+  </main>
+  <footer> {{footerText}} </footer>
+</div>
+</template>
+
+<script>
+// Imports
+export default {
+  name: 'Error404Page',
+  props: {
+    imageUrl: { type: String, default: 'Insert the image path to show it' },
+    title: { type: String, default: 'I have bad news for you' },
+    text: { type: String, default: 'The page you are looking for might be removed or is temporarily unavailable' },
+    buttonLabel: { type: String, default: 'Back to homepage' },
+    headerTitle: { type: String, default: '404 not found' },
+    footerText: { type: String, default: 'created by Leonardo - devChallenges.io' }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@700&family=Montserrat:wght@500;600;700&family=Space+Mono:wght@400;700&display=swap');
+
+@mixin display-flex-row {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+@mixin display-flex-column {
+      display: flex;
+      flex-direction: column;
+      align-content: center;
+}
+
+@mixin for-size($size) {
+  @if $size == min-320 {
+    @media screen and (min-width:320px) and (max-width: 480px) { @content; }
+  } @else if $size == min-481 {
+    @media screen and (min-width:481px) and (max-width: 768px) { @content; }
+  } @else if $size == min-769 {
+    @media screen and (min-width:769px) and (max-width: 1024px) { @content; }
+  }@else if $size == min-1024 {
+    @media screen and (min-width:1024px) and (max-width: 1500px) { @content; }
+  }
+}
+
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    height: 100vh;
+    #container-main{
+      width: 100vw;
+      @include display-flex-column();
+    }
+  header{
+      font-family: 'Inconsolata';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 24px;
+      line-height: 25px;
+      letter-spacing: -0.08em;
+      text-transform: uppercase;
+      color: #333333;
+      padding: 30px;
+    }
+  main{
+    @include display-flex-row;
+    width: 100vw;
+    margin: 5% 0;
+      @include for-size(min-320) { @include display-flex-column(); }
+      @include for-size(min-481) { @include display-flex-column(); }
+      @include for-size(min-769) { @include display-flex-column(); }
+    #container-left {
+      width: 50%;
+      height: 100%;
+      @include display-flex-row;
+      img {
+        max-width: 35vw;
+          @include for-size(min-320) { max-width: 60vw; }
+          @include for-size(min-481) { max-width: 60vw; }
+          @include for-size(min-769) { max-width: 46vw; }
+          @include for-size(min-1024) { max-width: 46vw; }
+      }
+    }
+    #container-right {
+      @include display-flex-column;
+      gap: 3vw;
+      h1 {
+        max-width: 35vw;
+        font-family: Space Mono;
+        font-size: 64px;
+        font-weight: 700;
+        line-height: 95px;
+        letter-spacing: -0.035em;
+        text-align: left;
+        margin: 0;
+        @include for-size(min-320) { max-width: 62vw; font-size: 44px; line-height: 54px;}
+        @include for-size(min-481) { max-width: 72vw; font-size: 44px; line-height: 54px;}
+        @include for-size(min-769) { max-width: 35vw; font-size: 32px; line-height: 40px; }
+        @include for-size(min-1024) { max-width: 35vw; font-size: 34px; line-height: 40px; }
+      }
+      p {
+        max-width: 23vw;
+        font-family: Space Mono;
+        font-size: 24px;
+        font-weight: 400;
+        line-height: 36px;
+        letter-spacing: -0.035em;
+        text-align: left;
+        margin: 0;
+        @include for-size(min-320) { max-width: 70vw; font-size: 18px; line-height: 27px; margin: 0 0 40px 0;}
+        @include for-size(min-481) { max-width: 70vw; font-size: 18px; line-height: 27px; margin: 0 0 40px 0;}
+        @include for-size(min-769) { max-width: 33vw; font-size: 16px; line-height: 36px;}
+      }
+      button {
+        width: 216px;
+        height: 68px;
+        background: #333333;
+        color: #fff;
+        font-family: Space Mono;
+        font-size: 14px;
+        font-weight: 700;
+        line-height: 21px;
+        letter-spacing: -0.035em;
+        text-transform: uppercase;
+      }
+    }
+  }
+  footer{
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 17px;
+    text-align: center;
+    color: #BDBDBD;
+  }
+}
+
+</style>
--- ui.frontend/src/map-components.js
+++ ui.frontend/src/map-components.js
@@ -1,13 +1,7 @@
-import CustomClasses from './components/CustomClasses/CustomClasses'
-import Text from './components/Text/Text'
-import AppPage from './components/Page/AppPage'
-import CheckBox from './components/Checkbox/Checkbox'
-import Header from './components/Header/Header'
-import FragmentText from './components/FragmentText/FragmentText'
-import ChallengeBtn from './components/ChallengeBtn/ChallengeBtn'
-import LoginChallenge from './components/LoginChallenge/LoginChallenge'
+import Error404Page from './components/Error404Page/Error404Page'
 import HomePage from './components/HomePage/HomePage'
-import Image from './components/Image/Image'
+import LoginChallenge from './components/LoginChallenge/LoginChallenge'
+import AppPage from './components/Page/AppPage'
 import {
   withComponentMappingContext,
   AllowedComponentsContainer,
@@ -28,43 +22,14 @@ const EditConfig = {
               ↓  NEW COMPONENTS MAPPING COME HERE  ↓
 **********************************************************************/
 
-MapTo('vue/components/customclasses')(CustomClasses, EditConfig)
-
-// Checkbox Component Mapping
-MapTo('vue/components/checkbox')(CheckBox, EditConfig)
-
-// Header Component Mapping
-MapTo('vue/components/header')(Header, EditConfig)
-
-// FragmentText Component Mapping
-MapTo('vue/components/fragmenttext')(FragmentText, EditConfig)
-
-// LoginChallenge Component Mapping
-MapTo('vue/components/loginchallenge')(LoginChallenge, EditConfig)
+// error404page Component Mapping
+MapTo('vue/components/error404page')(Error404Page, EditConfig)
 
 // homePage Component Mapping
 MapTo('vue/components/homePage')(HomePage, EditConfig)
 
-// ChallengeBtn Component Mapping
-MapTo('vue/components/challengebtn')(ChallengeBtn, EditConfig)
-
-// Image Component Mapping
-MapTo('vue/components/image')(Image, {
-  emptyLabel: 'Image',
-  isEmpty: function (props) {
-    return !props || !props.src || props.src.trim().length < 1
-  },
-  resourceType: 'vue/components/image'
-})
-
-// Text Component Mapping
-MapTo('vue/components/text')(Text, {
-  emptyLabel: 'Text',
-
-  isEmpty: function (props) {
-    return !props || !props.text || props.text.trim().length < 1
-  }
-})
+// LoginChallenge Component Mapping
+MapTo('vue/components/loginchallenge')(LoginChallenge, EditConfig)
 
 // Container Component Mapping
 MapTo('vue/components/container')(
